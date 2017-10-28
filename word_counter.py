aa46f508f50286dd97bf396fa05b44ab7da25d9a
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-10-28 10:52:57 +0200

    Implemented word counter

---
+++ word_counter.py
@@ -0,0 +1,13 @@
+from mrjob.job import MRJob
+
+class MRWordCount(MRJob):
+
+    def mapper(self, key, line):
+        for word in line.split():
+            yield (word.lower(), 1)
+
+    def reducer(self, word, count):
+        yield (word, sum(count))
+
+if __name__ == '__main__':
+    MRWordCount.run()
